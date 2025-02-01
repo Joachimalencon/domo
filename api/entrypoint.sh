3f081b86a5556fd2e1cd153fd3eba38c2c088b34
joachim <joachim.a@hotmail.fr>
2025-02-01 19:53:43 +0100

    Move composer install from dockerfile to entrypoint

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -6,6 +6,10 @@ until php bin/console doctrine:query:sql "SELECT 1" >/dev/null 2>&1; do
   sleep 2
 done
 
+if [ ! -d "vendor" ]; then
+  COMPOSER_ALLOW_SUPERUSER=1 composer install --no-interaction --optimize-autoloader
+fi
+
 php bin/console doctrine:database:create --if-not-exists
 
 php bin/console doctrine:migrations:migrate --no-interaction
